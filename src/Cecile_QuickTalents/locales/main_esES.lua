6cacf617cebb507563a80b51229ae41be482d189
Juan Medina <jamedina@gmail.com>
2016-12-19 00:11:36 +0000

    preparing release

--- src/Cecile_QuickTalents/locales/main_esES.lua
+++ src/Cecile_QuickTalents/locales/main_esES.lua
@@ -21,6 +21,24 @@ L["PROFILES"] = "Perfiles"
 L["BINDING_DESC"] = "Abrir Talentos Rápidos"
 L["ABOUT"] = [[|cff0070deAgradecimientos especiales para:|r
 
+Thereplicant, Abetø, Tárl, Krannt.
+
 |cff0070deUso:|r
+
+Este AddOn permite al usuario definer conjuntos de talentos por raid y jefe o instancias miticas +.
+
+Usando su interfaz configurable, con docenas de atajos, puedes cambiar de talentos rapidamente.
+
+Navega entre los diferentes jefes y activa un conjunto con solo unas pulsaciones de teclado.
+
+Otros atajos permite copiar y pegar talentos, o incluso usar tus 'Escrito de la mente tranquila'.
+
+De todas formas pudas usar tambien el raton, si preferes una forma mas tradicional de usar este interfaz.
+
+Cualquier comentario o sugerencia es bienvenido en las web abituales de AddOns.
+
+Para abir el interfaz puedes usar el atajo de teclado definido en esta opciones o escribir:
+
+|cff82c5ff/cqt ui|r
 
 ]]
